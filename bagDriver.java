b1f3c823dbfb30794e95ba0cfdae0f371d1af5f2
Eric Huang <erichuang54@gmail.com>
2022-02-24 02:04:22 -0800

    second commit

--- bagDriver.java
+++ bagDriver.java
@@ -3,10 +3,10 @@ public class bagDriver
     public static void main(String[] args)
     {
         BagInterface<String> bag1 = new ResizableArrayBag<>();
+        BagInterface<String> bag2 = new ResizableArrayBag<>();
         bag1.add("a");
         bag1.add("a");
         bag1.add("a");
         System.out.println(bag1.toArray());
-        System.out.println("HI");
     }
 }
